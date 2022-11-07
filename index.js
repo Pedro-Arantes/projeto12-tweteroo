86d4748e0455a5ca3b6b85f5bd63380761964e99
Pedro-Arantes <developer9pedro@gmail.com>
2022-11-07 13:39:29 -0300

    Version 1.2 Bonus included

--- index.js
+++ index.js
@@ -62,12 +62,38 @@ const tweets = [
 
 
 const users = [];
-let avata;
+const filterPages = (array,num) =>{
+	let elArray = []
+	let numb = num*10;
+	for (let index = 0; index < array.length; index++) {
+		const element = array[index];
+		elArray.push(element)
+		if (numb <=10 && elArray.length === numb-1) {
+			return elArray
+		}
+	}
+	
+	elArray = numb === 20 ? elArray.filter((item,id,arr)=>id>9&&id<20): elArray
+	
+	return elArray
+}
 
 app.get('/tweets', (req, res) => {
-    
-    res.send(tweets);
+	const { page } = req.query;
+    if (page < 1) {
+		res.status(400).send("Informe uma página válida!")
+		return;
+	}
+	const filteredTweets = filterPages(tweets,page)
+	console.log(filteredTweets)
+    res.send(filteredTweets);
   });
+
+app.get('/tweets/:username', (req, res) => {
+	const {username} = req.params
+    const filteredTweets = tweets.filter((item)=>item.username.toLowerCase() === username.toLowerCase())
+    res.send(filteredTweets);
+  }); 
 
 app.post('/sign-up', (req, res) => {
 	const {username,avatar} = req.body;
@@ -87,15 +113,16 @@ app.post('/sign-up', (req, res) => {
   });
 
   app.post('/tweets', (req, res) => {
-	const {username,tweet} = req.body;
+	const {tweet} = req.body;
+	const {user} = req.headers;
 
-	if (!username||!tweet) {
+	if (!user||!tweet) {
 		res.status(400).send("Todos os Campos são obrigatórios!");
 		return;
 	}
-	const avata = users.find((x)=>x.username === username)
+	const avata = users.find((item)=>item.username === user)
 	const obj = {
-		username: username,
+		username: user,
 		avatar: avata.avatar,
   		tweet: tweet
 	}
